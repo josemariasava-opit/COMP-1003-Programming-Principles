cf38b25b1e0a3d5caf610761f691301d756ded8f
josemariasava-opit <josemaria.s@students.opit.com>
2024-10-27 09:40:38 +0100

    Constant and define

---
+++ Constant.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+
+/*
+the name of the constant in c are always UPPERCASE
+*/
+
+#define PI 3.14 // Constant using #define - declaration oustside the main method 
+
+int main() {
+    const int MAX_VALUE = 100; // Constant declaration 
+    printf("Max Value: %d\n", MAX_VALUE);
+    printf("Value of PI: %f\n", PI);
+    
+    
+    
+    
+    return 0;
+}
